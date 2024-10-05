2862509c47acc3884b5f9317eb6d95a93a316223
Michael Dodis <michael.dodis@oramavr.com>
2024-10-05 09:35:25 +0200

    begin project file

---
+++ edlib/project.h
@@ -0,0 +1,11 @@
+#ifndef VDED_PROJECT_H
+#define VDED_PROJECT_H
+
+#include <common.h>
+
+typedef struct {
+    const char  *name;
+    u32         version;
+} VDED_Project;
+
+#endif // !VDED_PROJECT_H
